22648ad1c07f6fa2c3835b929be014e6c4e8c074
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-02 21:29:47 +0200

    Initial project

---
+++ src/main/java/com/example/springsecurityauthenticationmysql/SpringSecurityAuthenticationMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityauthenticationmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityauthenticationmysql/SpringSecurityAuthenticationMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityauthenticationmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthenticationMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
